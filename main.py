f4b12d095ff701755377bdf4cbefcf6a756ef866
awael <awael@aucegypt.edu>
2019-11-06 11:18:32 +0200

    now draws the components (seems to be correct but didnt check if sizing is valid) added comments about how to draw pins (and vias)

--- main.py
+++ main.py
@@ -20,19 +20,26 @@ lef_parser.parse()
 e = np.array(list(def_parser.nets))
 #print(e[0])
 
-
-print(def_parser.components.num_comps)
-for component in def_parser.components.comps:
-    print(component.name)
-    print(component.placed)
-    print(lef_parser.macro_dict[component.name.split("_")[0]].info['SIZE'])
-    print("----------------------------------")
-
 #d = draw.Drawing(height = def_parser.diearea[1][0]-def_parser.diearea[0][0], width = def_parser.diearea[1][1]-def_parser.diearea[0][1])
 d = draw.Drawing('./try this.svg', size = (def_parser.diearea[1][1]-def_parser.diearea[0][1],def_parser.diearea[1][0]-def_parser.diearea[0][0]))
 
-# draw a red box
-d.add(d.rect((100, 100), (300, 200),fill='blue',fill_opacity=0.5))
+
+#print(def_parser.components.num_comps)
+for component in def_parser.components.comps:
+    #print(component.name)
+    #print(component.placed)
+    #print(lef_parser.macro_dict[component.name.split("_")[0]].info['SIZE'])
+    #print("----------------------------------")
+    d.add(d.rect((component.placed[0], component.placed[1]), (lef_parser.macro_dict[component.name.split("_")[0]].info['SIZE'][0], lef_parser.macro_dict[component.name.split("_")[0]].info['SIZE'][1]), fill='#39ff14', fill_opacity=0.5))
+print(def_parser.nets.nets[580])
+#the above object has attributes:
+# .end_via
+# .layer
+# .end_via_loc
+# .points
+# .type, we need to use these with the svg element polyLine to create wires and place vias when needed in different colors.
+
+
 d.saveas("./try this.svg")
 
 
